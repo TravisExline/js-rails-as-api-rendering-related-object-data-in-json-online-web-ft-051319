6f38e5c6ff6fc001985bb34399be1316a89a5751
Travis Exline <t.exline22@gmail.com>
2019-08-22 14:55:48 +0000

    Automatically backed up by Learn

--- db/migrate/20190822145210_create_locations.rb
+++ db/migrate/20190822145210_create_locations.rb
@@ -1,8 +1,7 @@
 class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
-      t.string :latitude
-      t.string :float
+      t.float :latitude
       t.float :longitude
 
       t.timestamps
